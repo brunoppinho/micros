9ddf5a6bea5bdd6da8a76e988d7e91943659b293
bruno-pinho-alpe <bruno.pinho@alpenet.com.br>
2023-10-02 21:53:59 -0300

    Adicionando utilização Eureka;

--- pedido/src/main/java/tech/ada/pedido/service/RestTemplateValidarProduto.java
+++ pedido/src/main/java/tech/ada/pedido/service/RestTemplateValidarProduto.java
@@ -1,5 +1,7 @@
 package tech.ada.pedido.service;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,10 +11,20 @@ import tech.ada.produto.model.Produto;
 @ConditionalOnSingleCandidate(ValidarProduto.class)
 public class RestTemplateValidarProduto implements ValidarProduto {
 
+    private final EurekaClient eurekaClient;
+
+    public RestTemplateValidarProduto(EurekaClient eurekaClient) {
+        this.eurekaClient = eurekaClient;
+    }
+
     @Override
     public void execute(int produtoId) {
-        String hostName = "127.0.0.1";
-        String port = "8762";
+        InstanceInfo destination = eurekaClient.getApplication("produto")
+                .getInstances()
+                .get(0);
+
+        String hostName = destination.getHostName();
+        int port = destination.getPort();
         String url = "http://" + hostName + ":" + port + "/" + produtoId;
         new RestTemplate().getForObject(url, Produto.class);
     }
